219e1175adcccdf98be8726383f7d125c2b744c1
vee-infinite <vhouse@uw.edu>
2024-07-20 15:47:42 -0700

    Google API setup
    Added "tools" scene where I'm going to make the tool to get info from google sheets once we figure out format, added google api dlls, started the csv .cs file but it currently isn't seeing the dll assets.

---
+++ Team1FinalProject/Assets/Scripts/Tools/googleToCSV.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//commenting out the google libs because they don't work yet. 
+//using Google.Apis.Auth.OAuth2;
+//using Google.Apis.Sheets.v4;
+//using Google.Apis.Services;
+//using Google.Apis.Sheets.v4.Data;
+//using CsvHelper;
+//using CsvHelper.Configuration;
+using System.IO;
+using System;
+
+public class googleToCSV : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
