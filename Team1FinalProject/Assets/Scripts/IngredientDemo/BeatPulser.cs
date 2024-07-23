0394951c4f788b88069903563207a7df103f1274
rhonig22 <ryan.honig22@outlook.com>
2024-07-23 12:40:49 -0700

    Changing beatScroller to beatPulser

--- Team1FinalProject/Assets/Scripts/IngredientDemo/BeatPulser.cs
+++ Team1FinalProject/Assets/Scripts/IngredientDemo/BeatPulser.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BeatScroller : MonoBehaviour
+public class BeatPulser : MonoBehaviour
 {
     private float _beatTempo;
     public bool HasStopped { get; private set; }
