c42b834a2e6fef66424c09d88c32167ccf5a2e1c
rhonig22 <ryan.honig22@outlook.com>
2024-08-06 13:48:58 -0700

    Hit Text Refactor
    - Move hit texts to screenspace - overlay
    - position them with an offset centered around the ingredient box

--- Team1FinalProject/Assets/Scripts/UXControllers/HitTextUXController.cs
+++ Team1FinalProject/Assets/Scripts/UXControllers/HitTextUXController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class HitTextUXController : MonoBehaviour
 {
     [SerializeField] private CanvasGroup _canvasGroup;
+    [SerializeField] private TextMeshProUGUI _hitText;
     private readonly float _timeLength = 1.5f;
     private float _timeElapsed = 0;
 
@@ -14,5 +16,10 @@ public class HitTextUXController : MonoBehaviour
         _canvasGroup.alpha = 1 - _timeElapsed / _timeLength;
         if (_timeElapsed > _timeLength)
             Destroy(gameObject);
+    }
+
+    public void SetLocation(Vector3 pos)
+    {
+        _hitText.rectTransform.localPosition = pos;
     }
 }
