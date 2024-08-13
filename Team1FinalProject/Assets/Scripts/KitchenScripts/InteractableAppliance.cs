a31528fc12a84318d17e50def8af83e33b60176a
vee-infinite <vhouse@uw.edu>
2024-08-12 20:16:33 -0700

    Ingredient organization and (somewhat incorrect) placement
    We should make sure everyone agrees when the sprites should switch+how many button pushes there are

--- Team1FinalProject/Assets/Scripts/KitchenScripts/InteractableAppliance.cs
+++ Team1FinalProject/Assets/Scripts/KitchenScripts/InteractableAppliance.cs
@@ -33,6 +33,7 @@ public class InteractableAppliance : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+       
         if(collision.gameObject.CompareTag("Player"))
         {
             _isInRange = true;
