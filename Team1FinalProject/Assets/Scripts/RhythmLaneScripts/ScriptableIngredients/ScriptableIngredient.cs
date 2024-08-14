716bca79458e16171429200402f9ac81f3050371
vee-infinite <vhouse@uw.edu>
2024-08-13 18:39:48 -0700

    Adding background asset switch, inspecting tomato pixels, creating particles cs file

--- Team1FinalProject/Assets/Scripts/RhythmLaneScripts/ScriptableIngredients/ScriptableIngredient.cs
+++ Team1FinalProject/Assets/Scripts/RhythmLaneScripts/ScriptableIngredients/ScriptableIngredient.cs
@@ -7,6 +7,7 @@ public class ScriptableIngredient : ScriptableObject
     [SerializeField] private string _ingredientName;
     [SerializeField] private GameObject _ingredientPrefab;
     [SerializeField] private Sprite[] _ingredientSprites;
+    [SerializeField] private Sprite[] _backgroundPrepSprites;
 
     public string GetName()
     {
@@ -22,7 +23,25 @@ public class ScriptableIngredient : ScriptableObject
         else
             return _ingredientSprites[_ingredientSprites.Length - 1];
     }
+    public Sprite getBGPrepSprite(int index)
+    {
+        if (_backgroundPrepSprites == null || _backgroundPrepSprites.Length == 0)
+        {
 
+            Debug.Log("background null");
+            return null;
+        }
+        else if (index < _backgroundPrepSprites.Length)
+        {
+            Debug.Log("background shouldn't be null")
+;            return _backgroundPrepSprites[index];
+        }
+        else
+        {
+            Debug.Log("What happened here");
+            return _backgroundPrepSprites[_backgroundPrepSprites.Length - 1];
+        }
+    }
     public GameObject getPrefab()
     {
         return _ingredientPrefab;
