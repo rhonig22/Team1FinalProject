888624140f49de00b086ce286c1b251ebff5d28b
rhonig22 <ryan.honig22@outlook.com>
2024-08-27 17:08:03 -0700

    Calculate 3 Star Score as Percentage
    - Rather than a set 3 star score, calculate it as a percent of the max notes that the recipe has
    - Also perform some MixPanel logging

--- Team1FinalProject/Assets/Scripts/UXControllers/RecipeButtonController.cs
+++ Team1FinalProject/Assets/Scripts/UXControllers/RecipeButtonController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using mixpanel;
 
 public class RecipeButtonController : MonoBehaviour
 {
@@ -17,6 +18,9 @@ public class RecipeButtonController : MonoBehaviour
 
     public void RecipeClicked()
     {
+        var props = new Value();
+        props["recipeName"] = _recipe.GetName();
+        MixpanelLogger.Instance.LogEvent("Recipe Started", props);
         RecipeManager.Instance.SetRecipe(_recipe);
         GameManager.Instance.LoadKitchen();
     }
